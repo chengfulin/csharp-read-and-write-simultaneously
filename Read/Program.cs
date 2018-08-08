93af3c40196452b8b047530aa1fb2c655ff6cf71
ChengFuLin <chengfulin0806@gmail.com>
2018-08-09 00:53:55 +0800

    Setup

---
+++ Read/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Read
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(Path.GetFullPath(Path.Combine($"{AppDomain.CurrentDomain.BaseDirectory}", "..\\..\\..\\..")), "files\\out.json");
+
+            using (var file = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                while (true)
+                {
+                    var reader = new StreamReader(file);
+                    var line = reader.ReadLine();
+                    Console.WriteLine(line);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ Write/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Write
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(Path.GetFullPath(Path.Combine($"{AppDomain.CurrentDomain.BaseDirectory}", "..\\..\\..\\..")), "files\\out.json");
+            
+            using (var file = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read))
+            {
+                while (true)
+                {
+                    var writer = new StreamWriter(file);
+                    writer.WriteLine(DateTime.Now.ToLongTimeString());
+                    writer.Flush();
+                    Thread.Sleep(500);
+                }
+            }
+        }
+    }
+}
